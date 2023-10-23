689276ba59e42e58cd4232987d15bd4f9f9c0ce2
Angel Martinez <angel.martinez@monash.edu>
2023-10-23 15:06:46 +1100

    Updload main document

--- timedistance.py
+++ timedistance.py
@@ -28,58 +28,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 # import scipy.io as sp
 import time
+from datetime import datetime
 import polarTransform
 # from datetime import datetime
 # import matplotlib.patches as mpatches
 
 
-class cubo:
-    def getdata(self, array):
-        if isinstance(array, (str)):
-            cube = fits.getdata(array, 0)
-            return cube, None
-        elif isinstance(array, (int, float)):
-            return "Not a valid array"
-        else:
-            return NotImplemented
+# class cubo:
+#     def getdata(self, array):
+#         if isinstance(array, (str)):
+#             cube = fits.getdata(array, 0)
+#             return cube, None
+#         elif isinstance(array, (int, float)):
+#             return "Not a valid array"
+#         else:
+#             return NotImplemented
 
 
 class td:
 
     rsun_m = 695.5e6
+    fe = 3
+
     rsun_pix = 1884  # Value for HMI
 
     def __init__(self, array, x0=148, y0=140, theta0=192, theta1=260,
                  path=None, radius=None, radius0=0, time1=False,
                  time0=False, cartesian=None, xticks=10, yticks=6):
         '''
-            timedistance.td
-            ===============
+        timedistance.td
+        ===============
 
-            Python class that calls the initial parameters for the TD plot.
+        Python class that calls the initial parameters for the TD plot.
 
-            The class functions are tdplot, test, slider, and cbar_slider.
+        The class functions are tdplot, test, slider, and cbar_slider.
 
-                tdplot:
-                =======
+            tdplot:
+            =======
 
-                Computes the TD diagram provided the datacube in fits format.
+            Computes the TD diagram provided the datacube in fits format.
 
-                Example: A datacube `DOPP.fits` has a suspected sunquake in the
-                coordinates x:520 y:210 between two arcsections th0:45deg and
-                th1:72deg, extending out to 150pix in the image. To calculate
-                the TD associated the procedure is:
+            Example: A datacube `DOPP.fits` has a suspected sunquake in the
+            coordinates x:520 y:210 between two arcsections th0:45deg and
+            th1:72deg, extending out to 150pix in the image. To calculate
+            the TD associated the procedure is:
 
-                    >>> from timedistance import td
-                    >>> data = td('DOPP.fits', x0=520, y0=210, \
-                    >>>     theta0=45, theta1=72, radius=150)
-                    >>> data.plot()
+                >>> from timedistance import td
+                >>> data = td('DOPP.fits', x0=520, y0=210, \
+                >>>     theta0=45, theta1=72, radius=150)
+                >>> data.plot()
 
-                Additional arguments can be parsed to plot such as
-                norm=True      - To normalize intensity images,
-                intensity=True - To show intensity images
-                magneto=True   - Shows colorbar for magnetograms
-                colorbar=True  - To show colorbar
+            Additional arguments can be parsed to plot such as
+            norm=True      - To normalize intensity images,
+            intensity=True - To show intensity images
+            magneto=True   - Shows colorbar for magnetograms
+            colorbar=True  - To show colorbar
         '''
 
         self.array = array
@@ -98,7 +101,7 @@ class td:
 
     # Function that makes the time-distance plot
 
-    def tdplot(self, colorbar=False, params=True, norm=False, cmap='gray',
+    def tdplot(self, colorbar=True, norm=False, cmap='gray',
                interpolation='sinc', **kwargs):
 
         self.colorbar = colorbar
@@ -107,13 +110,17 @@ class td:
         # self.save = save
         # self.notime = notime
         # self.title = title
-        self.params = params
+        # self.params = params
         # self.magneto = magneto
         # self.intensity = intensity
         self.cmap = cmap
 
         # Call the array
-        cube = cubo().getdata(self.array)
+        # cube = cubo().getdata(self.array)
+        hdul = fits.open(self.array)
+        hdr = hdul[0].header
+        data = hdul[0].data
+
         s = time.time()
 
         # Convert the angles to radians
@@ -122,9 +129,11 @@ class td:
 
         # To ensure the minimum radius in the plot
         # This is to set finalRadius in converting to polar coordinates
+        naxis1 = hdr['NAXIS1']
+        naxis2 = hdr['NAXIS2']
         if self.radius is None:
-            mx = cube[0][0].shape[0]/2
-            my = cube[0][0].shape[1]/2
+            mx = naxis1/2
+            my = naxis2/2
             if (self.x0 <= mx) and (self.y0 <= my):
                 rad = min(self.x0, self.y0)
             elif (self.y0 > my) and (self.x0 <= mx):
@@ -135,26 +144,29 @@ class td:
                 rad = min(mx*2 - self.x0, my*2 - self.y0)
         else:
             rad = self.radius
+        rad = int(rad)
 
-        # If put, the final image will be up the selected one,
-        # otherwise it uses the entire range-1
+        # First frame of the TD plot
         if self.time0:
             image0 = self.time0
         else:
             image0 = 0
 
+        # Last frame of the plot. The plot range will be time1 - time0
         if self.time1:
             image1 = self.time1
         else:
-            image1 = len(cube[0])-1
+            image1 = len(data)-1
 
         # td graph
         image_td = []
         for ij in range(image0, image1):
             polarImage, ptSettings = polarTransform.convertToPolarImage(
-                cube[0][ij], center=[self.x0, self.y0],
+                data[ij], center=[self.x0, self.y0],
                 initialRadius=self.radius0, finalRadius=rad,
-                initialAngle=t0, finalAngle=t1)
+                initialAngle=t0, finalAngle=t1,
+                radiusSize=rad-self.radius0, order=0,
+                )
             slices = []
             for i in range(len(polarImage[0, :])):
                 slices.append(np.mean(polarImage[:, i]))
@@ -165,31 +177,38 @@ class td:
         # Variables for the graph. The final graph will be shown if is
         # selected the keyword "graph", and will be saved if the keyword
         # "save" is selected
-        if params:
-            plt.rcParams.update({'font.size': 12})
-            plt.subplots_adjust(right=0.96, bottom=0.12, top=0.93, left=0.11)
-        else:
-            pass
+        # if params:
+        #     plt.rcParams.update({'font.size': 12})
+        #     plt.subplots_adjust(right=0.96, bottom=0.12, top=0.93, left=0.11)
+        # else:
+        #     pass
 
         plt.xlabel("Distance (Mm)")
         plt.ylabel("Time (min)")
         # Every 4 pixels in PT correstpond to 1 pix in Postel
-        rad_Pos = rad*335/240  # Here I convert from postel pix to PT
-        pix_PT = np.arange(0, rad_Pos, self.xticks*4)
-        pix_Mm = np.arange(0, rad_Pos/4, self.xticks, dtype=int)
-        plt.xticks(pix_PT, pix_Mm)
+        # rad_Pos = rad*335/240  # Here I convert from postel pix to PT
+        # pix_PT = np.arange(0, rad_Pos, self.xticks*4)
+        # pix_Mm = np.arange(0, rad_Pos/4, self.xticks, dtype=int)
+        # plt.xticks(pix_PT, pix_Mm)
         plt.yticks(np.arange(0, 240, 40/3), np.arange(0, 180, 10))
-        plt.title(self.title)
+
+        t_rec = datetime.now().strftime(r"%Y-%m-%d %H:%M:%S")
+
+        try:
+            t_rec = hdr['T_REC']
+        except KeyError:
+            print(f'T_REC keyword not found in header. Set to {t_rec}')
 
         if norm:
             image_td /= np.max(image_td)
 
-        plt.imshow(image_td, origin='lower', interpolation=interpolation,
-                   aspect='auto', cmap=self.cmap, **kwargs)
+        im = plt.imshow(image_td, origin='lower', interpolation=interpolation,
+                        aspect='auto', cmap=self.cmap, **kwargs)
         plt.tick_params(direction='out', length=6, width=1.0, colors='k',
                         grid_color='yellow', grid_alpha=0.99)
 
-        # if self.colorbar:
+        if self.colorbar:
+            plt.colorbar(im, label=r'$\Delta$ V$_{LOS}$ (m/s)')
         #     if self.intensity:
         #         plt.colorbar(label=r'$\Delta$ Counts', pad=0.03, aspect=18)
         #     elif self.magneto:
@@ -300,9 +319,9 @@ class td:
                 plt.ylabel(" ")
                 plt.imshow(final, cmap='Greys_r', origin='lower',
                            interpolation='spline36', aspect='auto')
-                ax.text(0.45, 0.1, "x="+str(X0)+"\ny="+str(Y0),
-                        bbox=dict(boxstyle="round", ec='k', fc=(1, 1, 1, 0.6)),
-                        transform=ax.transAxes)
+                ax.text(0.45, 0.1, f"x={X0}\ny={Y0}", transform=ax.transAxes,
+                        bbox=dict(boxstyle="round", ec='k',
+                                  fc=(1, 1, 1, 0.6)))
         e = time.time()
         print("\n Time spent in test: {:0.2f} s".format(e-s))
         plt.show()
@@ -392,8 +411,9 @@ class td:
         image = td(self.array, x0=self.x0, y0=self.y0, theta0=self.theta0,
                    theta1=self.theta1, time0=self.time0, time1=self.time1,
                    radius=self.radius, path=self.path, radius0=self.radius0)
-        im = np.array(image.tdplot(plot=False, colorbar=False,
-                                   params=False))
+        # im = np.array(image.tdplot(plot=False, colorbar=False,
+        im = np.array(image.tdplot(colorbar=False))
+        #                            params=False))
 
         im1 = ax.imshow(im, cmap='Greys_r', origin='lower', aspect='auto',
                         interpolation='spline36', **kwargs)
@@ -421,31 +441,39 @@ class td:
 if __name__ == "__main__":
 
     #  Initial parameters
-    name = 'hmi_data/DOPP_20110730-M9_3-DIFF.fits'  # Name of datacube
-    x0 = 495   # x-center
-    y0 = 510   # y-center
-    th0 = 0    # Initial angle
-    th1 = 40   # End angle
+    # name = 'hmi_data/DOPP_20110730-M9_3-DIFF.fits'  # Name of datacube
+    name = './DOPP_DIFF.fits'  # Name of datacube
+    x0 = 503   # x-center
+    y0 = 512   # y-center
+    th0 = 194    # Initial angle
+    th1 = 258   # End angle
 
-    rad0 = 25   # Skip these pixels in the TD plot
-    rad1 = 150  # Final radius (distance) in pixels
+    rad0 = 0   # Skip these pixels in the TD plot
+    rad1 = 200  # Final radius (distance) in pixels
 
-    time0 = 5   # Initial frame for calculations
-    time1 = 90  # Final frame for calculations
+    time0 = 100   # Initial frame for calculations
+    time1 = 200  # Final frame for calculations
 
     # Calculates the TD diagram (calls the td class instance with init values)
-    data = td(name, x0=x0, y0=y0, theta0=th0, theta1=th1,
-              radius0=rad0, radius=rad1, time0=time0, time1=time1)
+    data = td(name, x0=x0, y0=y0, theta0=th0, theta1=th1, radius0=rad0,
+              radius=rad1, time0=time0, time1=time1)
 
+    plt.figure()
+    plt.subplot(111)
+    image = data.tdplot(colorbar=True, vmin=-510, vmax=510,
+                        interpolation='sinc')
+    print(image.shape)
+    plt.show()
+
+    # plt.show()
+    # exit()
     # Plots the TD diagram
-    plot = data.tdplot(colorbar=True, plot=True, vmin=-510, vmax=510,
-                       title='2011-07-30 M9.3 T02:09')
 
     # Slider to change the colorbar limits (improve contrast)
-    data.cbar_slider()
+    # data.cbar_slider()
 
     # Interactive slides to best fit the TD
-    data.slider()
+    # data.slider()
 
     # Plot many around
     # data.test(rows=3,columns=5)
