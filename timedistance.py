f1d23a2414df96b0893a731c58eac2b720fa56d7
Angel Martinez <angel.martinez@monash.edu>
2023-10-26 10:26:20 +1100

    Version 01
    Updated modular structure

--- timedistance.py
+++ timedistance.py
@@ -83,7 +83,7 @@ class td:
     rsun_pix = 1884  # Value for HMI
 
     def __init__(self, array, x0, y0, theta0, theta1, radius0=0, radius=None,
-                 time0=0, time1=None):
+                 time0=0, time1=None, readcube=True):
         '''
         Arc section parameters for the TD plot.
 
@@ -132,12 +132,15 @@ class td:
         self.theta1 = theta1
         self.radius = radius
         self.radius0 = radius0
+        self.read_cube = readcube
 
-        data, hdr = read_cube(array)
-        if time1 is None:
-            time1 = data.shape[0]
+        if readcube:
+            global data, hdr
+            data, hdr = read_cube(array)
         self.data = data
         self.hdr = hdr
+        if time1 is None:
+            time1 = data.shape[0]
         image_td, polarImage, ptSettings = calc_td(data, hdr, x0, y0, theta0,
                                                    theta1, radius0, radius,
                                                    time0, time1)
@@ -175,16 +178,16 @@ class td:
         except KeyError:
             print(f'DAXIS3 keyword not found in header. Set to {daxis3}')
 
-        plt.xlabel("Distance (Mm)")
-        plt.ylabel("Time (min)")
+        plt.gcf()
         extent = [0, (self.radius-self.radius0)*daxis*td.r_sun, 0,
                   (self.time1-self.time0)*45/60]
-
         im = plt.imshow(self.image_td, origin='lower', aspect='auto',
                         interpolation=interpolation, cmap=self.cmap,
                         extent=extent, **kwargs)
         plt.tick_params(direction='out', length=6, width=1.0, colors='k',
                         grid_color='yellow', grid_alpha=0.99)
+        plt.xlabel("Distance (Mm)")
+        plt.ylabel("Time (min)")
 
         if self.colorbar:
             plt.colorbar(im, label=r'$\Delta$ V$_{LOS}$ (m/s)')
@@ -211,6 +214,95 @@ class td:
             To handle color contrast (min and max of colorbar)
 
     """
+
+    def slider(self, **kwargs):
+
+        # from matplotlib.widgets import Slider
+        from matplotlib.widgets import TextBox
+
+        def data_params(x0, y0, t0, t1, radius0):
+            td_im = td(self.array, x0=x0, y0=y0, theta0=t0, theta1=t1,
+                       radius0=radius0, radius=self.radius,
+                       time0=self.time0, time1=self.time1, readcube=False)
+            return td_im.image_td
+
+        first_image = data_params(self.x0, self.y0, self.theta0, self.theta1,
+                                  self.radius0)
+
+        print('\n\n ', self.radius0, self.radius)
+        fig = plt.figure(figsize=(10, 7))
+        ax = plt.subplot(121)
+        # ax = plt.axes([0.25, 0.25, 0.6, 0.6])
+        global im_plot
+        im_plot = ax.imshow(first_image, cmap='gray', origin='lower',
+                            interpolation='spline36', **kwargs)
+        print(first_image.shape)
+        plt.rcParams.update({'font.size': 12})
+        # Set up sliders
+        # axt1 = plt.axes([0.10, 0.06, 0.33, 0.04], facecolor='lightcyan')
+        # axt0 = plt.axes([0.10, 0.11, 0.33, 0.04], facecolor='pink')
+        # axy0 = plt.axes([0.59, 0.11, 0.33, 0.04], facecolor='lightcyan')
+        # axx0 = plt.axes([0.59, 0.06, 0.33, 0.04], facecolor='pink')
+        # axrd = plt.axes([0.10, 0.89, 0.34, 0.04], facecolor='pink')
+
+        # slider_t1 = Slider(axt1, r'$\theta _1(^\circ)$', self.theta1-30,
+        #                    self.theta1+30, valinit=self.theta1)
+        # slider_t0 = Slider(axt0, r'$\theta _0(^\circ)$', self.theta0-30,
+        #                    self.theta0+30, valinit=self.theta0)
+        # slider_x0 = Slider(axy0, 'x (pix)', self.x0-30, self.x0+30,
+        #                    valinit=self.x0, dragging=False)
+        # slider_y0 = Slider(axx0, 'y (pix)', self.y0-30, self.y0+30,
+        #                    valinit=self.y0)
+        # slider_r0 = Slider(axrd, 'radio', 0, 50, valinit=self.radius0)
+
+        # # Update the image
+        # def update(val):
+        #     radius0 = slider_r0.val
+        #     theta0 = slider_t0.val
+        #     theta1 = slider_t1.val
+        #     x0 = slider_x0.val
+        #     y0 = slider_y0.val
+        #     im_plot.set_data(data_params(int(x0), int(y0), int(theta0),
+        #                                  int(theta1), int(radius0)))
+        #     fig.canvas.draw_idle()
+        #     # fig.canvas.draw()
+        #     # plt.clear()
+        #     plt.draw()
+
+        def submit(expression):
+            theta0 = eval(expression, {'theta0': self.theta0})
+            x0 = eval(expression, {'x0': str(self.y0)})
+            # y0 = eval(expression, {'y0': self.y0})
+            # theta1 = eval(expression, {'t1': self.theta1})
+            print(theta0, x0)
+            print('\n \n \n \n')
+            im_plot.set_data(data_params(x0, self.y0, theta0,
+                                         self.theta1, 0))
+            fig.canvas.draw_idle()
+            # ax.relim()
+            # ax.autoscale_view()
+            plt.draw()
+
+        # axbox = fig.add_axes([0.1, 0.05, 0.8, 0.075])
+        axx0 = plt.axes([0.10, 0.11, 0.33, 0.04], facecolor='pink')
+        axy0 = plt.axes([0.10, 0.06, 0.3, 0.04], facecolor='lightcyan')
+        axt0 = plt.axes([0.59, 0.06, 0.33, 0.04], facecolor='pink')
+        axt1 = plt.axes([0.59, 0.11, 0.33, 0.04], facecolor='lightcyan')
+        text_box_x0 = TextBox(axx0, r"$x_0$ ", textalignment="center")
+        text_box_y0 = TextBox(axy0, r"$y_0$ ", textalignment="center")
+        text_box_t0 = TextBox(axt0, r"$\theta_0$ ", textalignment="center")
+        text_box_t1 = TextBox(axt1, r"$\theta_0$ ", textalignment="center")
+        text_box_x0.on_submit(submit)
+        # text_box_y0.on_submit(submit)
+        text_box_t0.on_submit(submit)
+        # text_box_t1.on_submit(submit)
+        text_box_x0.set_val(self.x0)  # Trigger submit with initial string.
+        # text_box_y0.set_val(self.y0)
+        text_box_t0.set_val(self.theta0)
+        # text_box_t1.set_val(self.theta1)
+
+        plt.show()
+        # del im_plot
 
     # Makes a test of many pixels around (5 columns and 4 rows)
     # Reference to IDL procedure (Martinez-Oliveros)
@@ -257,79 +349,6 @@ class td:
                                   fc=(1, 1, 1, 0.6)))
         plt.show()
 
-    def slider(self, **kwargs):
-
-        from matplotlib.widgets import Slider  # , Button, RadioButtons
-
-        # Image configuration and first call
-        # plt.figure(figsize=(13,9))
-        # fig, ax = plt.subplots()
-        # fig.subplots_adjust(bottom=0.15)
-
-        def data(x0, y0, t0, t1, radius0):
-            image = td(self.array, x0=int(x0), y0=int(y0), theta0=int(t0),
-                       theta1=int(t1), radius0=int(radius0),
-                       radius=self.radius, time0=self.time0, time1=self.time1)
-            im = image.tdplot(plot=False, colorbar=False, params=False,
-                              **kwargs)
-            return im
-
-        im = data(self.x0, self.y0, self.theta0, self.theta1, self.radius0)
-        image = plt.imshow(im, cmap='Greys_r', interpolation='spline36',
-                           origin='lower')
-
-        # Remove actual image to not be shown
-        # plt.show(block=False)
-        # plt.pause(0.0001)
-        plt.close('all')
-
-        plt.figure(figsize=(10, 7))
-        plt.rcParams.update({'font.size': 12})
-        # Set up sliders
-        axt1 = plt.axes([0.10, 0.06, 0.33, 0.04], facecolor='lightcyan')
-        axt0 = plt.axes([0.10, 0.11, 0.33, 0.04], facecolor='pink')
-        axy0 = plt.axes([0.59, 0.11, 0.33, 0.04], facecolor='lightcyan')
-        axx0 = plt.axes([0.59, 0.06, 0.33, 0.04], facecolor='pink')
-        axrd = plt.axes([0.10, 0.89, 0.34, 0.04], facecolor='pink')
-
-        slider_r0 = Slider(axrd, 'radio', 0, 50, valinit=self.radius0)
-        slider_t1 = Slider(axt1, r'$\theta _1(^\circ)$', self.theta1-30,
-                           self.theta1+30, valinit=self.theta1)
-        slider_t0 = Slider(axt0, r'$\theta _0(^\circ)$', self.theta0-30,
-                           self.theta0+30, valinit=self.theta0)
-        slider_x0 = Slider(axy0, 'x (pix)', self.x0-30, self.x0+30,
-                           valinit=self.x0, dragging=False)
-        slider_y0 = Slider(axx0, 'y (pix)', self.y0-30, self.y0+30,
-                           valinit=self.y0)
-
-        plt.axes([0.25, 0.25, 0.6, 0.6])
-
-        # Update the image
-        def update(val):
-            global image
-            theta0 = slider_t0.val
-            theta1 = slider_t1.val
-            x0 = slider_x0.val
-            y0 = slider_y0.val
-            radius0 = slider_r0.val
-            image.set_data(data(int(x0), int(y0), int(theta0), int(theta1),
-                                int(radius0)))
-            # fig.canvas.draw()
-            # plt.clear()
-            plt.draw()
-
-        # Change sliders
-        slider_r0.on_changed(update)
-        slider_t0.on_changed(update)
-        slider_t1.on_changed(update)
-        slider_x0.on_changed(update)
-        slider_y0.on_changed(update)
-
-        plt.show()
-        del image
-
-    # Visualization of the time distance plot cahnging (not necessary)
-
     def cbar_slider(self, **kwargs):
 
         from matplotlib.widgets import Slider  # , Button, RadioButtons
@@ -370,8 +389,11 @@ class td:
 
 if __name__ == "__main__":
 
-    #  Initial parameters
-    # name = 'hmi_data/DOPP_20110730-M9_3-DIFF.fits'  # Name of datacube
+    from timedistance import td
+    import os
+
+    os.system('clear')
+
     name = './DOPP_DIFF.fits'  # Name of datacube
     x0 = 503  # x-center
     y0 = 512  # y-center
@@ -388,28 +410,59 @@ if __name__ == "__main__":
     data = td(name, x0=x0, y0=y0, theta0=th0, theta1=th1, radius0=rad0,
               radius=rad1, time0=time0, time1=time1)
 
-    fig = plt.figure(figsize=(12, 5))
-    plt.subplots_adjust(wspace=0.2, left=0.1, right=0.95)
-
-    plt.subplot(121)
-    image = data.tdplot(colorbar=False, vmin=-300, vmax=300,
-                        interpolation='sinc')
-
-    plt.subplot(122)
-    angle1, rho1, time1, ampl1 = np.loadtxt('./../../../test_bc_filter/td/OUT',
-                                            unpack=True)
-    r_sun = 696  # Mm
-    frame0 = 30
-    image = data.tdplot(colorbar=False, vmin=-300, vmax=300,
-                        interpolation='sinc')
-    # print(im)
-    plt.ylabel(' ')
-    plt.plot(rho1*r_sun, (time1 + frame0*45)/60)
-    plt.xlim(0, image.shape[1]*0.0005*r_sun)
-    plt.ylim(0, image.shape[0]*45/60)
-    # cb_ax = fig.add_axes([0.83, 0.1, 0.02, 0.8])
-    # cbar = fig.colorbar(im, cax=cb_ax)
+    # plt.figure()
+    # plt.subplot(121)
+    # image = data.plot(colorbar=True, vmin=-300, vmax=300,
+    #                   interpolation='sinc')
+    #
+    # plt.show()
+    # exit()
+    image_cart = data.toCartesian
+    data.slider()
     plt.show()
+    exit()
+
+    print('End')
+    # #  Initial parameters
+    # # name = 'hmi_data/DOPP_20110730-M9_3-DIFF.fits'  # Name of datacube
+    # name = './DOPP_DIFF.fits'  # Name of datacube
+    # x0 = 503  # x-center
+    # y0 = 512  # y-center
+    # th0 = 194  # Initial angle
+    # th1 = 258  # End angle
+
+    # rad0 = 0  # Skip these pixels in the TD plot
+    # rad1 = 200  # Final radius (distance) in pixels
+
+    # time0 = 100  # Initial frame for calculations
+    # time1 = 200  # Final frame for calculations
+
+    # # Calculates the TD diagram (calls the td class inst with init values)
+    # data = td(name, x0=x0, y0=y0, theta0=th0, theta1=th1, radius0=rad0,
+    #           radius=rad1, time0=time0, time1=time1)
+
+    # fig = plt.figure(figsize=(12, 5))
+    # plt.subplots_adjust(wspace=0.2, left=0.1, right=0.95)
+
+    # plt.subplot(121)
+    # image = data.tdplot(colorbar=False, vmin=-300, vmax=300,
+    #                     interpolation='sinc')
+
+    # plt.subplot(122)
+    # angle1, rho1, time1, ampl1 = np.loadtxt(
+    #     './../../../test_bc_filter/td/OUT', unpack=True)
+    # r_sun = 696  # Mm
+    # frame0 = 30
+    # image = data.tdplot(colorbar=False, vmin=-300, vmax=300,
+    #                     interpolation='sinc')
+    # # print(im)
+    # plt.ylabel(' ')
+    # plt.plot(rho1*r_sun, (time1 + frame0*45)/60)
+    # plt.xlim(0, image.shape[1]*0.0005*r_sun)
+    # plt.ylim(0, image.shape[0]*45/60)
+    # # cb_ax = fig.add_axes([0.83, 0.1, 0.02, 0.8])
+    # # cbar = fig.colorbar(im, cax=cb_ax)
+    # plt.show()
 
     # plt.show()
     # exit()
